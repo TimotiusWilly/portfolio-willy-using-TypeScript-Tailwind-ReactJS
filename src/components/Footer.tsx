5f2fc708cfc9fc4aed3637d0f30fab71b24eec8b
WillyNaren <timotiuswillynarendra12@gmail.com>
2026-04-19 13:54:34 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,16 +9,16 @@ const Footer = () => {
             <a href="#">Proyek</a>
         </div>
         <div className="flex items-center gap-3">
-            <a href="#">
+            <a href="https://github.com/TimotiusWilly?tab=repositories" target="_blank" rel="noopener noreferrer">
                 <i className="ri-github-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://www.instagram.com/willynaren/" target="_blank" rel="noopener noreferrer">
                 <i className="ri-instagram-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://web.facebook.com/timotius.willy.363979?_rdc=1&_rdr#" target="_blank" rel="noopener noreferrer">
                 <i className="ri-facebook-fill ri-2x"></i>
             </a>
-            <a href="#">
+            <a href="https://discord.com/users/willygg0887" target="_blank" rel="noopener noreferrer">
                 <i className="ri-discord-fill ri-2x"></i>
             </a>
         </div>
